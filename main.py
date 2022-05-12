42d6e8c62b1752d71ad7a73c447f1198a23880aa
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-12 19:54:51 +0200

    .

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def changeError(string):
     #error_msg.pack_forget()
     error_msg.config(state="disabled")
     print(string)
+    print(inp1)
 
 def isZero(string1):
     string1 = string1.replace("\n","")
