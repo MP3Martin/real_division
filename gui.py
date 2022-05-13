3fb0ebd9bd335a146e20133a263b2434b08994bb
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-13 21:13:31 +0200

    .

--- gui.py
+++ gui.py
@@ -207,7 +207,6 @@ master.bind("<Control-Key-A>", select_all)
 
 #CTRL+C FUNCTION
 def copy_selected(event):
-    # iprint(event.widget.get("1.0", "end-1c"))
     selected_text = event.widget.get(tk.SEL_FIRST, tk.SEL_LAST)
     selected_text = str(selected_text)
     selected_text = selected_text.replace("\n", "")
@@ -222,7 +221,6 @@ master.bind("<Control-Key-C>", copy_selected)
 
 #CTRL+X FUNCTION
 def cut_selected(event):
-    # iprint(event.widget.get("1.0", "end-1c"))
     selected_text = event.widget.get(tk.SEL_FIRST, tk.SEL_LAST)
     selected_text = str(selected_text)
     selected_text = selected_text.replace("\n", "")
