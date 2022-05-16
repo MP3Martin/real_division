0b15042fe5a46c95384eae7c57b22d99ac44568f
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-16 18:09:05 +0200

    .

--- real_division_core/__init__.py
+++ real_division_core/__init__.py
@@ -5,20 +5,27 @@ def calc(one, two):
 
     #Setup
     firstLine = f"{one} : {two} = "
+    result = ""
     lines = []
+    numbersUsed = 0
 
     #Functions
-    def howManyTimesDoesItFit(one1, two1):
+    def howManyTimesDoesItFit(one1: int, two1: int):
         temp_one = one1
         temp_two = two1
         one1 = temp_two
         two1 = temp_one
         return math.floor(one1 / two1)
 
-    print(howManyTimesDoesItFit(3,25))
+    def getFirstXChars(string: str, x: int):
+        return string[:x]
+
+    #The calculation
+    while numbersUsed < one:
+        if 
 
     # Put everything together
-    out = firstLine
+    out = firstLine + result
     for line in lines:
         out = f"{out}\n{line}"
 
