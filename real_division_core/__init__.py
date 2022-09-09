f9e9a3881ac1eccf36e0428cde2c68b5f4169458
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-09-09 19:27:27 +0200

    Working way better

--- real_division_core/__init__.py
+++ real_division_core/__init__.py
@@ -22,9 +22,52 @@ def calc(one, two):
     def getFirstXChars(string: str, x: int):
         return string[:x]
 
+    def firstPosThatFits(x: int, y: int):
+        second_num = 0
+        for i in range(1, len(str(y)) + 1):
+            second_num = int(str(y)[:i])
+            if x <= second_num:
+                break 
+        return second_num
+
+
     #Calc
-    out = "ez"
-    return out
+    leftNums = []
+    leftNums.append(one)
+
+    firstLeftNum = firstPosThatFits(two, leftNums[-1])
+    one_unselected = list(str(one)[len(str(firstLeftNum)):])
+    one_unselected = [eval(i) for i in one_unselected]
+
+    result = ""
+    while True:
+        leftNum = firstPosThatFits(two, leftNums[-1])
+        result += str(leftNum // two)
+        remains = str(leftNum % two)
+        try:
+            newLeftNum = remains + str(one_unselected[0])
+            one_unselected.pop(0)
+        except IndexError:
+            newLeftNum = remains
+        
+        leftNums.append(int(newLeftNum))
+
+        if str(result) == str(one // two) and str(leftNums[-1]) == str(one % two):
+            break
+
+    #Generate output
+    leftNums.pop(0)
+    leftNums = [str(i) for i in leftNums]
+
+    # tempLeftNums = leftNums
+    # tempLeftNums.pop()
+    # for i in range(len(tempLeftNums)):
+    #     tempLeftNums[i] = tempLeftNums[i].replace("0", "00")
+    # leftNums = tempLeftNums.append(leftNums[-1])
+
+    #TODO: Replace all inbetween strings in leftNums with replace("0", "00")
+
+    return result + f"\n{leftNums}"
 
 if __name__ == '__main__':
     print("This program is not meant to be used standalone")
