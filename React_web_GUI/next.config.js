134c78c8ffa5a00306bc815cac3996c35aa90fad
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-09-18 15:58:10 +0200

    FIx nextjs not building

--- React_web_GUI/next.config.js
+++ React_web_GUI/next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
     }
     return config
   },
-  future: {
-    webpack5: true,
-  },
+  // future: {
+  //   webpack5: true,
+  // },
   reactStrictMode: true,
   swcMinify: true,
 }
