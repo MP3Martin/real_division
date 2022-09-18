dd2c2167dde465cc233cd8e5c66ddd3b42720e7f
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-09-18 17:07:27 +0200

    Move scripts

--- React_web_GUI/pages/index.js
+++ React_web_GUI/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import {Accordion} from 'react-bootstrap';
 
 export default function Home() {
@@ -8,15 +9,27 @@ export default function Home() {
       <Head>
         <title>NextJS app with react-bootstrap</title>
         {/* <script src="/lib/brython-runner.bundle.js"></script> */}
-        <script src="/lib/brython.js"></script>
-        <script src="/lib/brython_stdlib.js"></script>
-        <script src="/lib/run_python.js"></script>
-        <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+      </Head>
+
+      <Script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+        window.brythonLoadedNumber = 0;
+        window.brythonReallyLoaded = function(){}
+        window.brythonLoaded = function() {
+          window.brythonLoadedNumber =  window.brythonLoadedNumber + 1
+          if (window.brythonLoadedNumber > 2) {
+            window.brythonReallyLoaded()
+          }
+        }
+        `}} strategy="beforeInteractive"/>
+      <Script src="/lib/brython.js" strategy="beforeInteractive" onLoad={() => {window.brythonLoaded()}}/>
+      <Script src="/lib/brython_stdlib.js" strategy="beforeInteractive" onLoad={() => {window.brythonLoaded()}}/>
+      <Script src="/lib/run_python.js" strategy="beforeInteractive" onLoad={() => {window.brythonLoaded()}}/>
+      <Script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+        window.brythonReallyLoaded = function() {
           void(0)
           brython({pythonpath: ["/lib/"], indexedDB: false});
-          `}}>
-        </script>
-      </Head>
+        }
+        `}} strategy="beforeInteractive"/>
 
       <main>
         <h1>
