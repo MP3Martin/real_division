e79018b60e3d075de3472b7665be35608d1a5e55
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-10-01 20:57:01 +0200

    Fix gh icon being small all the time

--- NextJS_web_GUI/components/NavBarRightIcons.js
+++ NextJS_web_GUI/components/NavBarRightIcons.js
@@ -1,8 +1,7 @@
-import {React, Component} from 'react';
+import {React, Component, useState, useEffect} from 'react';
 import styled from 'styled-components';
 import IconButton from '@mui/material/IconButton';
 import Constants from '../constants.json'
-import { useWindowSize } from '@react-hook/window-size'
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 
@@ -11,16 +10,18 @@ const StyledWrapper = styled.div`
 `;
 
 function NavBarRightIcons(props){
+    function iconButtonSize(width) {
+        if (width < 360) { return "small" }
+        return "large"
+    }
 
-    const [width, height] = useWindowSize()
-
-    var iconButtonSize = "large"
-    if (width < 360) { iconButtonSize = "small" }
+    // the following line exists thanks to https://remotestack.io/react-js-detect-window-width-and-height-tutorial/
+    const[windowDimenion,detectHW]=useState({winWidth:window.innerWidth,winHeight:window.innerHeight}),detectSize=()=>{detectHW({winWidth:window.innerWidth,winHeight:window.innerHeight})};useEffect((()=>(window.addEventListener("resize",detectSize),()=>{window.removeEventListener("resize",detectSize)})),[windowDimenion]);
 
     return(
         <StyledWrapper style={props.style}>
             {/* <div style={{"height": "52px", "width": "52px"}} /> */}
-            <IconButton size={iconButtonSize} href={Constants.adress.github} target="_blank">
+            <IconButton size={iconButtonSize(windowDimenion.winWidth)} href={Constants.adress.github} target="_blank">
                 <GitHubIcon fontSize="inherit" />
             </IconButton>
 
