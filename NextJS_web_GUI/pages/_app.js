8d3b288ad8f75606a67a365ffeac439acf05aa53
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-10-02 12:13:05 +0200

    Add loading animation + add global animation delay

--- NextJS_web_GUI/pages/_app.js
+++ NextJS_web_GUI/pages/_app.js
@@ -1,19 +1,46 @@
-import Head from 'next/head';
-import '../styles/css.scss'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import NavBar from '../components/NavBar';
+import { React, Component, useState, useEffect } from "react";
+import Head from "next/head";
+import "../styles/css.scss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import NavBar from "../components/NavBar";
+import LoadingOverlay from "react-loading-overlay";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setLoading] = useState(true);
+  const [isMainVisible, setMainVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 10);
+    setMainVisible(true);
+  });
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <LoadingOverlay
+        active={isLoading}
+        spinner
+        text="Loading the page..."
+        fadeSpeed={200}
+      >
+        <main style={{ height: "100vh", "animation-delay": "5s" }}>
+          {isMainVisible && (
+            <>
+              <NavBar />
+              <Component {...pageProps} />
+            </>
+          )}
+        </main>
+      </LoadingOverlay>
       <Head>
         <link
           rel="stylesheet"
@@ -27,15 +54,13 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
-
+        <link
+          href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <main>
-        <NavBar />
-        <Component {...pageProps} />
-      </main>
     </ThemeProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
