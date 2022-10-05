c0a05d08887f4eee73c2e01412c73faff634307c
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-10-05 20:49:15 +0200

    Update index.js

--- NextJS_web_GUI/pages/index.js
+++ NextJS_web_GUI/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Script from 'next/script';
 import { useState, useEffect } from 'react';
-import {inDevEnvironment} from '../public/lib/DevEnv';
+import { inDevEnvironment } from '../public/lib/DevEnv';
 import Input from '../components/input/input'
 
 import { setGlobalState, useGlobalState } from '../hooks/globalState';
+import useDeviceSize from './../hooks/useDeviceSize';
 
 export default function Home() {
 
@@ -18,16 +19,16 @@ export default function Home() {
   })
 
   return (
-    <div>
+    <>
       <Head>
         <title>NextJS app with react-bootstrap</title>
       </Head>
 
-      {inDevEnvironment || <Script src="https://cdn.jsdelivr.net/gh/MP3Martin/jsRUNpy.js@1/jsRUNpy.min.js" strategy="afterInteractive" onLoad={()=> {setBrLibsLoaded(old => [...old, ..."c"])}}/>} {/* prod */}
-      {inDevEnvironment && <Script src="/lib/jsRUNpy.js" strategy="afterInteractive" onLoad={()=> {setBrLibsLoaded(old => [...old, ..."c"])}}/>} {/* dev */}
-      
+      {inDevEnvironment || <Script src="https://cdn.jsdelivr.net/gh/MP3Martin/jsRUNpy.js@1/jsRUNpy.min.js" strategy="afterInteractive" onLoad={() => { setBrLibsLoaded(old => [...old, ..."c"]) }} />} {/* prod */}
+      {inDevEnvironment && <Script src="/lib/jsRUNpy.js" strategy="afterInteractive" onLoad={() => { setBrLibsLoaded(old => [...old, ..."c"]) }} />} {/* dev */}
+
       <Input />
 
-    </div>
+    </>
   )
 }
