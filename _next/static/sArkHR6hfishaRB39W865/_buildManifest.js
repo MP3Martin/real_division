acbac34ae084c4c739967973dbcbc64a31b42d6d
MP3Martin <MP3Martin@users.noreply.github.com>
2022-10-06 18:20:22 +0000

    Deploying to gh-pages from @ MP3Martin/real_division@9877a50e9f9eecadfbe1e2739b14e4c3909be1df 🚀

---
+++ _next/static/sArkHR6hfishaRB39W865/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/726-1af42429926a4ab5.js","static/chunks/958-a2e25adf5b006612.js","static/chunks/pages/index-e77c744cb6e684ba.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
