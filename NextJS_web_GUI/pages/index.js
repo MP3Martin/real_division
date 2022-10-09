16125f4e7a2fd55084f2f5fd9539c4eafe438fbd
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-10-09 20:41:04 +0200

    Add sussy egg

--- NextJS_web_GUI/pages/index.js
+++ NextJS_web_GUI/pages/index.js
@@ -24,6 +24,11 @@ export default function Home() {
         <title>NextJS app with react-bootstrap</title>
       </Head>
 
+        {/* <Script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/egg.js/1.0/egg.min.js" onLoad={()=>{
+          var egg = new Egg("up,up", function() {
+            alert("never")
+          }).listen();
+        }} /> */}
       {inDevEnvironment || <Script src="https://cdn.jsdelivr.net/gh/MP3Martin/jsRUNpy.js@1/jsRUNpy.min.js" strategy="afterInteractive" onLoad={() => { setBrLibsLoaded(old => [...old, ..."c"]) }} />} {/* prod */}
       {inDevEnvironment && <Script src="/lib/jsRUNpy.js" strategy="afterInteractive" onLoad={() => { setBrLibsLoaded(old => [...old, ..."c"]) }} />} {/* dev */}
 
