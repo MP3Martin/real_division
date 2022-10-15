d363d128376e32572a13124b90e9144bb29c230f
MP3Martin <MP3Martin@users.noreply.github.com>
2022-10-15 10:39:35 +0000

    Deploying to gh-pages from @ MP3Martin/real_division@9fc0728e868e395fab7ab7d63a6147b2db2a3e02 🚀

--- _next/static/giwLmVoeqw_ebf7-dQo3m/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/56-afe8215c96bb0630.js","static/chunks/68-58ef491570f72629.js","static/chunks/pages/index-c1fe0d54ab0cdff5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/oaGYCmCtFDViaeqg4v4gM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/56-afe8215c96bb0630.js","static/chunks/68-58ef491570f72629.js","static/chunks/pages/index-090ead4a3e66cab2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
