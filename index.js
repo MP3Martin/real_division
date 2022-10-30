ef9483b6c1e4171c05a31be33ac988fc0479d84f
MP3Martin <MP3Martin@users.noreply.github.com>
2022-10-30 13:50:13 +0000

    Deploying to gh-pages from @ MP3Martin/real_division@aebefd3f855c6b9213cb8904f7a3a28dcbbb0a0b 🚀

---
+++ index.js
@@ -0,0 +1,15 @@
+root = document.documentElement
+
+notHoveringAnyNBRI = () => { return Array.from(document.querySelectorAll(".navBarRightIconsChild:hover")).length == 0 }
+
+addEventListener('mousemove', (event) => {
+  if (notHoveringAnyNBRI()) {
+    setTimeout(() => {
+      if (notHoveringAnyNBRI()) {
+        root.style.setProperty('--nbri-are-small', 1);
+      }
+    }, 200)
+  } else {
+    root.style.setProperty('--nbri-are-small', 0.8);
+  }
+});
